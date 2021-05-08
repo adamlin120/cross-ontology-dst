6797b2e14162db51922cdf0f8a4a8d066202bbd0
YenTing (Adam) Lin <31605305+adamlin120@users.noreply.github.com>
2021-05-08 16:14:07 +0800

    :sparkles: WIP turn dataset

--- train_sgd.sh
+++ train_sgd.sh
@@ -1,4 +1,4 @@
-python -m schema_guided_dst.baseline.train_and_predict \
+python -m baseline.train_and_predict \
         --bert_ckpt_dir cased_L-12_H-768_A-12/ \
         --dstc8_data_dir dstc8-schema-guided-dialogue/ \
         --dialogues_example_dir dialogues_example/ \
